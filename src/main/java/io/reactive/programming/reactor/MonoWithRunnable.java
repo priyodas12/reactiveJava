181564f7149f8767847f5b1bda36b3d52e046685
Priyobrato Das <priyodas12@gmail.com>
2021-12-21 21:23:52 +0530

    d_12212021:mono with runnable implementation.

---
+++ src/main/java/io/reactive/programming/reactor/MonoWithRunnable.java
@@ -0,0 +1,25 @@
+package io.reactive.programming.reactor;
+
+import io.reactive.programming.reactor.util.SubscriberUtility;
+import reactor.core.publisher.Mono;
+
+public class MonoWithRunnable {
+    public static void main(String[] args) {
+
+        Runnable runnable = () -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            System.out.println("Process took time but Nothing to return");
+        };
+
+        Mono.fromRunnable(runnable)
+                .subscribe(
+                     SubscriberUtility.onNext()
+                    ,SubscriberUtility.onError()
+                    ,SubscriberUtility.onComplete()
+                );
+    }
+}
