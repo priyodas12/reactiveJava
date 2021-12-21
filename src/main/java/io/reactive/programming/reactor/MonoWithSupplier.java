c77d2792372b1e3339e44137932a8c7caffeac4e
Priyobrato Das <priyodas12@gmail.com>
2021-12-21 20:37:22 +0530

    d_12212021:mono with supplier implementation

---
+++ src/main/java/io/reactive/programming/reactor/MonoWithSupplier.java
@@ -0,0 +1,22 @@
+package io.reactive.programming.reactor;
+
+import com.github.javafaker.Faker;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+public class MonoWithSupplier {
+    public static void main(String[] args) {
+        //eager loading:1st line of getName() will execute
+        //Mono<String> simpleMono=Mono.just(getName());
+
+        //until there is no subscriber any line of getName() will not execute
+        Mono<String> supplierMono=Mono.fromSupplier(()->getName());
+        supplierMono.subscribe(System.out::println);
+    }
+
+    private static String getName(){
+        System.out.println("Generating name..."+new Date());
+        return Faker.instance().funnyName().name();
+    }
+}
