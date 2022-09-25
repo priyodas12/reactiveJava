19cc1da2c504eb10cf43646eb188de9126a7b96b
Priyobrato Das <priyodas12@gmail.com>
2022-09-25 22:11:51 +0530

    on error fix

--- src/main/java/io/reactive/programming/reactor/flux/customized/CustomizedFlux05.java
+++ src/main/java/io/reactive/programming/reactor/flux/customized/CustomizedFlux05.java
@@ -9,7 +9,10 @@ public class CustomizedFlux05 {
         Flux.generate(synchronousSink ->{
                 synchronousSink.next(Faker.instance().ancient().god().toString());
                 //synchronousSink.next(Faker.instance().ancient().god().toString());//more than one not allowed
-                synchronousSink.complete(); // complete() will pass single element then take() will not able to pass 1st 3 elements
+
+                //synchronousSink.error(new RuntimeException("synchronousSink error!"));
+
+                //synchronousSink.complete(); // complete() will pass single element then take() will not able to pass 1st 3 elements
                 })
                 .take(10)//provide limit unless it will be keep on emitting
                 .subscribe(new DefaultSubscriber("SUB-02")
