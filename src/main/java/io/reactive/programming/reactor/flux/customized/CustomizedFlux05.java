bb520ea27b48f49eee85888e37d27e50e83c2f0f
Priyobrato Das <priyodas12@gmail.com>
2022-09-25 22:05:42 +0530

    generate method

---
+++ src/main/java/io/reactive/programming/reactor/flux/customized/CustomizedFlux05.java
@@ -0,0 +1,19 @@
+package io.reactive.programming.reactor.flux.customized;
+
+import com.github.javafaker.Faker;
+import reactor.core.publisher.Flux;
+
+public class CustomizedFlux05 {
+    public static void main(String[] args) {
+
+        Flux.generate(synchronousSink ->{
+                synchronousSink.next(Faker.instance().ancient().god().toString());
+                //synchronousSink.next(Faker.instance().ancient().god().toString());//more than one not allowed
+                synchronousSink.complete(); // complete() will pass single element then take() will not able to pass 1st 3 elements
+                })
+                .take(10)//provide limit unless it will be keep on emitting
+                .subscribe(new DefaultSubscriber("SUB-02")
+                );
+
+    }
+}
