c71e997c4bd1d5d09bcb96a1e2b2f544ac226cb3
Priyobrato Das <priyodas12@gmail.com>
2022-09-25 19:02:47 +0530

    stock impl

---
+++ src/main/java/io/reactive/programming/reactor/flux/StockSubscription.java
@@ -0,0 +1,42 @@
+package io.reactive.programming.reactor.flux;
+
+import com.github.javafaker.Faker;
+import io.reactive.programming.reactor.flux.utility.FluxSubscriber;
+import io.reactive.programming.reactor.mono.util.SubscriberUtility;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class StockSubscription {
+    public static void main(String[] args) throws InterruptedException {
+        AtomicInteger at=new AtomicInteger(100);
+
+        List<Integer> list=new ArrayList<>();
+        Flux.interval(Duration.ofSeconds(1))
+                .map(i-> at.getAndAccumulate(FluxSubscriber.faker().random().nextInt(-5,5), Integer::sum))
+                .subscribe(
+                        (i)->{
+                            System.out.println(LocalDateTime.now()+"-------->"+i);
+                            list.add(i);
+                            if(i<50){
+                                System.out.println("Matched: "+i);
+                            }
+                        }
+                        , RuntimeException::new
+                        ,() -> {
+                            System.out.println("MAX PRICE::"+list.stream().max(Comparator.naturalOrder()));
+                            System.out.println("MIN PRICE::"+list.stream().min(Comparator.naturalOrder()));
+                            System.out.println("Completed");
+                        });
+
+        FluxSubscriber.sleep(100);
+    }
+}
