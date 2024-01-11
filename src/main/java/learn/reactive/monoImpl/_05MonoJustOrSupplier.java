eb691f092fb194e9fa0d290f187913e2e43568d3
Priyobrato.Das <priyodas12@gmail.com>
2024-01-12 00:02:34 +0530

    feature:mono just anf supplier

--- src/main/java/learn/reactive/monoImpl/_05MonoJustOrSupplier.java
+++ src/main/java/learn/reactive/monoImpl/_05MonoJustOrSupplier.java
@@ -6,11 +6,29 @@
 
 package learn.reactive.monoImpl;
 
+import learn.reactive.monoImpl.faker.FakerUtil;
+import reactor.core.publisher.Mono;
+
 public class _05MonoJustOrSupplier {
 
   public static void main(String[] args) {
 
+    //Mono.just()->if you have some data ready to publish
+    Mono<String> derivedName=Mono.just(getNames());
+    derivedName.subscribe(
+        FakerUtil.onNext()
+    );
 
+    //Supplier could be used to provide values lazily
+    Mono<String> supplierMono=Mono.fromSupplier(_05MonoJustOrSupplier::getNames);
+    supplierMono.subscribe(
+        FakerUtil.onNext()
+    );
+  }
+
+  public static String getNames(){
+    System.out.println("name generating...");
+    return FakerUtil.getFakeInstance().name().nameWithMiddle();
   }
 
 }
